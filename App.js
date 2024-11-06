98c2cb59174fcd69b2647cf27dc22f4483e3cbde
Bora Kaan Aras <115552817+borakaanaras@users.noreply.github.com>
2024-11-06 16:24:52 +0300

    Update App.js

--- App.js
+++ App.js
@@ -7,44 +7,46 @@ import Products_data from './patistore-items.json';
 const App = () => {
 
   const [text, onChangeText] = React.useState('Ara...');
-  const renderPati = ({item}) => <Products pati={item} />
 
-  
+  const renderPati = ({item}) => <Products pati={item} />;
+
   return (
-    <SafeAreaView style = {style.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Text style = {style.headerText}>MyAppyStore</Text>
-      <TextInput style = {style.textInput} ></TextInput>
+      <Text style={styles.headerText}>MyAppyStore</Text>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={onChangeText}
+        placeholder="Ara..."
+      />
       <FlatList
-      keyExtractor={item.id.toString()}
-        data = {Products_data}
+        keyExtractor={item => item.id.toString()}
+        data={Products_data}
         renderItem={renderPati}
         numColumns={2}
-    />
-
-
+      />
     </SafeAreaView>
-  )
+  );
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-
   },
   headerText: {
     fontWeight: 'bold',
     fontSize: 30,
     color: 'purple',
-    padding: 5
+    padding: 5,
   },
   textInput: {
     height: 40,
     backgroundColor: '#dcdcdc',
     padding: 10,
-    borderRadius: '5'
+    borderRadius: 5,  
+  },
+});
 
-  }
-})
 export default App;
